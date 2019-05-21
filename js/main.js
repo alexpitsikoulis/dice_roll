d87a9b4e198d91728fc1b4b6871839c6743019fe
Alex Pitsikoulis <alex.pitsikoulis@gmail.com>
2019-05-21 18:39:44 -0400

    created dice roll

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function rollDice() {
+    var reset = document.getElementsByTagName('div')[0];
+    reset.removeChild(reset.childNodes[0]);
+    var sides = document.getElementsByTagName('select')[0].value;
+    var num = Math.random()*sides;
+    var dice = Math.ceil(num);
+    var result = document.createElement('li');
+    result.innerHTML = dice;
+    document.getElementsByTagName('div')[0].appendChild(result);
+};
+var button = document.getElementsByTagName('button')[0];
+button.addEventListener('click', rollDice);
